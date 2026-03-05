09ee4857c41affaf881d6d3978e672b34a53b411
zhongzhw <dayanhn@126.com>
2026-03-05 19:29:27 +0800

    add doc

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./bazel-7.4.1-linux-x86_64 build --compilation_mode=dbg --copt=-g --copt=-O0 --strip=never //xla/examples/axpy:priority_fusion_analyzer //xla/examples/axpy:stablehlo_gpu_compiler //xla/examples/axpy:gpu_run_hlopasses //xla/examples/axpy:gpu_run_backend
+./bazel-7.4.1-linux-x86_64 build --compilation_mode=dbg --copt=-g --copt=-O0 --strip=never //xla/examples/axpy:priority_fusion_analyzer //xla/examples/axpy:stablehlo_gpu_compiler //xla/examples/axpy:gpu_run_hlopasses //xla/examples/axpy:gpu_run_backend //xla/examples/axpy:gpu_run_executable
